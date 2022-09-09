bb5d478b2b45583b27082e9484cfee65d168df63
Edgar Muñoz <edgmuo1085@gmail.com>
2022-09-09 07:12:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'appNotif',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
